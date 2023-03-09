edb272ba2d39837fa5d599e249e9c17786bd1cc7
Salvacasani <salvacasani@gmail.com>
2023-03-09 17:28:43 -0500

    Create FigureS6.R

---
+++ FigureS6.R
@@ -0,0 +1,42 @@
+##################################################################
+## Generate barplots on MDM2 Promoter-Enhancer categories, 
+## and overlaps with hubs, p53 and YY1
+
+
+lps141.mdm2.total <- 1414
+lps141.mdm2.promoters <- 711
+lps141.mdm2.enhancers <- 606
+
+
+lps853.mdm2.total <- 678
+lps853.mdm2.promoters <- 371
+lps853.mdm2.enhancers <- 271
+
+
+hct116.mdm2.total <- 142
+hct116.mdm2.promoters <- 49
+hct116.mdm2.enhancers <- 67
+  
+  
+u2os.mdm2.total <- 86
+u2os.mdm2.promoters <- 32
+u2os.mdm2.enhancers <- 46
+
+
+
+
+bar.mat <- cbind(c(lps141.mdm2.promoters/lps141.mdm2.total,lps141.mdm2.enhancers/lps141.mdm2.total),
+      c(lps853.mdm2.promoters/lps853.mdm2.total,lps853.mdm2.enhancers/lps853.mdm2.total),
+      c(hct116.mdm2.promoters/hct116.mdm2.total,hct116.mdm2.enhancers/hct116.mdm2.total),
+      c(u2os.mdm2.promoters/u2os.mdm2.total,u2os.mdm2.enhancers/u2os.mdm2.total))
+
+colnames(bar.mat) <- c('LPS141','LPS853','HCT116','U2OS')
+rownames(bar.mat) <- c('Promoters','Enhancers')
+
+pdf(file = 'promoter_enhancer_mdm2_barplots/stacked_barplot_enhProm.pdf', height = 5, width = 5)
+barplot(bar.mat,  names=c('LPS141','LPS853','HCT116','U2OS'))
+dev.off()
+
+pdf(file = 'promoter_enhancer_mdm2_barplots/besides_barplot_enhProm.pdf', height = 5, width = 5)
+barplot(bar.mat, beside=T,  names=c('LPS141','LPS853','HCT116','U2OS'))
+dev.off()
